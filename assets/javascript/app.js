ca2790c4c10ca016f7fc6ebdec470f24221a41f4
Nick Ramsay <NickRamsay1989@gmail.com>
2019-08-25 22:25:55 +1000

    fixed bug with gameOver function, adjusted formatting

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,6 +19,7 @@ $(document).ready(function () {
         }
     }
 
+
     var timeRemaining;
     var countdownTimer;
 
@@ -51,9 +52,6 @@ $(document).ready(function () {
         $(".gameContent").empty();
         clearInterval(countdownTimer);
         startCountDown();
-        if (questionHistory.length === questionCount) {
-            gameOverScreen();
-        } //If all questions have been used, clear history and restart selection process
         for (i = 0; (questionHistory.length === 0 && i === 0) || (questionHistory.length > 0 && questionHistory.indexOf(questionPicker) !== -1); i++) {
             questionPicker = Math.round(Math.random(questionCount));
         } //If history empty, iterate once & pick one. Else, continue picking until you find question NOT in history (indexOf = -1)
@@ -66,11 +64,11 @@ $(document).ready(function () {
         var timerDiv = $('<p align="center">Time Remaining: <span id="timer"></span></p>');
         $(".gameContent").append(timerDiv);
         $("#timer").text(timeRemaining);
-        
+
         var questionDiv = $('<h3 id="question"></h3>');
         $(".gameContent").append(questionDiv);
         $("#question").text(questions[currentQuestion].question);
-        
+
         var aoDiv1 = $('<h2 class="answer" id="answerOption1" data-question="answerOption1"></h2>');
         $(".gameContent").append(aoDiv1);
         $("#answerOption1").text(questions[currentQuestion].answerOption1);
@@ -103,51 +101,68 @@ $(document).ready(function () {
     var correctScreenTimer;
     var correctCount = 0;
 
-    function correctScreen () {
+    function correctScreen() {
         correctCount++;
         clearInterval(countdownTimer);
         $(".gameContent").empty();
-        var winMessage = $('<h2> Congratulations! ' + questions[currentQuestion].correctAnswer + ' was the right answer</h2>');
+        var winMessage = $('<h2> Congratulations! ' + questions[currentQuestion].correctAnswer + ' was the correct answer.</h2>');
         $(".gameContent").append(winMessage);
-        correctScreenTimer = setInterval(newQuestion, 1000 *5);
+        if (questionHistory.length < questionCount) {
+            correctScreenTimer = setInterval(newQuestion, 1000 * 5);
+        } else {
+            gameOverScreen();
+        }
     }
 
     var lossScreenTimer;
     var lossCount = 0;
 
-    function lossScreen () {
+    function lossScreen() {
         lossCount++;
         clearInterval(countdownTimer);
         $(".gameContent").empty();
-        var lossMessage = $('<h2> Sorry, wrong answer! You guessed '+ questions[currentQuestion][selectedAnswer] + '. However, '  + questions[currentQuestion].correctAnswer + ' was the right answer.</h2>');
+        var lossMessage = $('<h2> Sorry, wrong answer! You guessed ' + questions[currentQuestion][selectedAnswer] + '. The correct answer was ' + questions[currentQuestion].correctAnswer + '.</h2>');
         $(".gameContent").append(lossMessage);
-        lossScreenTimer = setInterval(newQuestion, 1000 *5);
+        if (questionHistory.length < questionCount) {
+            lossScreenTimer = setInterval(newQuestion, 1000 * 5);
+        } else {
+            gameOverScreen();
+        }
     }
 
     var timeoutTimer;
     var timeoutCount = 0;
 
-    function timeoutScreen () {
+    function timeoutScreen() {
         timeoutCount++;
         $(".gameContent").empty();
         var timeoutMessage = $('<h2>You\'ve run out of time! The correct answer was ' + questions[currentQuestion].correctAnswer + '. </h2>');
         $(".gameContent").append(timeoutMessage);
-        timeoutTimer = setInterval(newQuestion, 1000 *5);
+        if (questionHistory.length < questionCount) {
+            timeoutTimer = setInterval(newQuestion, 1000 * 5);
+        } else {
+            gameOverScreen();
+        }
     }
 
     var gameOverTimer;
 
-    function gameOverScreen () {
+    function gameOverScreen() {
         $(".gameContent").empty();
         clearInterval(countdownTimer);
-        var gameOverMessage = $('<h2>Game Over</h2><p>Correct Guesses: ' + correctCount + '</p> <p>Wrong Guesses: ' + lossCount + '</p> <p>Out of Time: ' + timeoutCount + '</p>');
+        var gameOverMessage = $('<h2>Game Over</h2><p>Correct Guesses: ' + correctCount + '</p> <p>Wrong Guesses: ' + lossCount + '</p> <p>Out of Time: ' + timeoutCount + '</p> <button class="btn btn-success newGameBtn">Play Again</button>');
         $(".gameContent").append(gameOverMessage);
-        gameOverTimer = setInterval(newQuestion, 1000 * 10);
         questionHistory = [];
         timeoutCount = 0;
         correctCount = 0;
         lossCount = 0;
+        $(".newGameBtn").on("click", function start() {
+            newQuestion();
+        })
     }
 
-    window.onload = newQuestion();
+    $(".startBtn").on("click", function start() {
+        newQuestion();
+    })
+
 })
